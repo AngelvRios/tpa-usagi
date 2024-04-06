ab7d3cb6ec1a913da5ed7371e57ad70dec32ea2d
AngelvRios <128487739+AngelvRios@users.noreply.github.com>
2024-04-06 17:48:19 -0300

    .

--- main.py
+++ main.py
@@ -20,19 +20,12 @@ class VentanaPrincipal(QMainWindow):
         b3 = QPushButton("3")
         b4 = QPushButton("4")
         b5 = QPushButton("5")
-        b6 = QPushButton("6")
-        b7 = QPushButton("7")
-        b8 = QPushButton("8")
-        b9 = QPushButton("9")
         caja.addWidget(b1, 0,0)
         caja.addWidget(b2, 0,1)
         caja.addWidget(b3, 0,2)
         caja.addWidget(b4, 1,0)
         caja.addWidget(b5, 1,1)
-        caja.addWidget(b6, 1,2)
-        caja.addWidget(b7, 2,0)
-        caja.addWidget(b8, 2, 1)
-        caja.addWidget(b9, 2, 2)
+
         """
         #Ejecucion de la reacción al Click
         b1.clicked.connect(self.reaccionar_1)
@@ -40,10 +33,6 @@ class VentanaPrincipal(QMainWindow):
         b3.clicked.connect(self.reaccionar_3)
         b4.clicked.connect(self.reaccionar_4)
         b5.clicked.connect(self.reaccionar_5)
-        b6.clicked.connect(self.reaccionar_6)
-        b7.clicked.connect(self.reaccionar_7)
-        b8.clicked.connect(self.reaccionar_8)
-        b9.clicked.connect(self.reaccionar_9)
         """
 
         #Ejecucion de la reacción al Click
@@ -52,10 +41,7 @@ class VentanaPrincipal(QMainWindow):
         b3.clicked.connect(lambda evento: self.reaccionar(b3.text()))
         b4.clicked.connect(lambda evento: self.reaccionar(b4.text()))
         b5.clicked.connect(lambda evento: self.reaccionar(b5.text()))
-        b6.clicked.connect(lambda evento: self.reaccionar(b6.text()))
-        b7.clicked.connect(lambda evento: self.reaccionar(b7.text()))
-        b8.clicked.connect(lambda evento: self.reaccionar(b8.text()))
-        b9.clicked.connect(lambda evento: self.reaccionar(b9.text()))
+
         panel = QWidget()
         panel.setLayout(caja)
         self.setCentralWidget(panel)
@@ -76,17 +62,7 @@ class VentanaPrincipal(QMainWindow):
         print(f"Boton 4")
     def reaccionar_5(self):
         print(f"Boton 5")
-    def reaccionar_6(self):
-        print(f"Boton 6")
 
-    def reaccionar_7(self):
-        print(f"Boton 7")
-
-    def reaccionar_8(self):
-        print(f"Boton 8")
-
-    def reaccionar_9(self):
-        print(f"Boton 9")
 
     
 
